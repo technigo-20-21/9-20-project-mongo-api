5afd72ee779455f281b8217c1916f73b26981a57
Evelina Berg <evelina.berg@outlook.com>
2020-12-20 12:18:31 +0100

    Error handling and query param added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv from 'dotenv'
 
 dotenv.config();
 
@@ -12,13 +12,21 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares to enable cors, json body parsing and error handling
 app.use(cors());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(500).json({ error: "Service unavailable" });
+  }
+});
 
+// Object model
 const Book = new mongoose.model("Member", {
   bookID: Number,
   title: String,
@@ -32,6 +40,7 @@ const Book = new mongoose.model("Member", {
   textReviewsCount: Number,
 });
 
+// Clearing and populating database
 if (process.env.RESET_DATABASE) {
   const populateDatabase = async () => {
     await Book.deleteMany();
@@ -44,7 +53,30 @@ if (process.env.RESET_DATABASE) {
   populateDatabase();
 }
 
-// Start defining your routes here
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to Evelina's RESTful book reviews API");
+});
+
+app.get("/books", async (req, res) => {
+  const books = await Book.find(req.query);
+  console.log(books);
+  res.json(books);
+});
+
+app.get("/books/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (book) {
+      res.json(book);
+    } else {
+      res.status(404).json({ error: "Book not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ error: "Invalid book id" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
